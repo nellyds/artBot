529dcd3cd258f255cc9575c58333b2364cca5cf7
Nelson D'Silva <ndsilva822@gmail.com>
2019-10-26 13:09:47 -0700

    added some error handling

--- App.py
+++ App.py
@@ -1,28 +1,21 @@
 import tweepy
 import csv
 import os
-<<<<<<< HEAD
+
 consumer_key = ''
 consumer_key_secret = ''
 access_token = ''
 access_token_secret = ''
-=======
-consumer_key = 'SDffI3QFeng4u5dWG9iEKqdqD'
-consumer_key_secret = 'm9hPW9TSdUX9ma6QaONN0p3hADe5KsBm1Vh9Vjoq5VXjvkHfQ6'
-access_token = '1179929860480872448-XQGmTnVI1wPPC0iTtkU9ya5Awqch1K'
-access_token_secret = 'OKk7YF3tx3yDuBu5U7fcGsoB3vZPG0MSXQHnjMnh5tiwn'
->>>>>>> 9d9e9b1a6e1ed6902bb98150ad3cde4a4033c7aa
+
 
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_key_secret)
 auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth)
 # note your output folder here
-<<<<<<< HEAD
+
 workDir = ""
-=======
-workDir = "/Users/nelson/Documents/illustrations"
->>>>>>> 9d9e9b1a6e1ed6902bb98150ad3cde4a4033c7aa
+
 # verify connectivity
 try:
     api.verify_credentials()
@@ -47,12 +40,15 @@ def writeCurrent():
         writer.writerows(x for x in files)
 
 def lookForNewest():
-    return [x for x in readCsv('current.csv') if x not in readCsv('newest.csv')]
+    if os.path.exists('current.csv'):
+        return [x for x in readCsv('current.csv') if x not in readCsv('newest.csv')]
+    return [x for x in readCsv('newest.csv')]
 
 def toTweetOrNot(artOutput):
     if len(artOutput) > 0:
         for x in artOutput:
-            api.media_upload(x, 'This is what I have been up to, hope you like it!')
+            print(workDir + "/" + x[:-1])
+            api.media_upload(workDir + "/" +x[:-1], 'This is what I have been up to, hope you like it!')
     else:
         api.update_status("I am a bad artist who hasn't made anything new.")
 
@@ -61,15 +57,10 @@ def main():
     writeCurrent()
     newArt = lookForNewest()
     toTweetOrNot(newArt)
-    os.rename('newest.csv','newcurrent.csv')
-    os.remove('current.csv')
-    os.rename('newcurrent.csv','current.csv')
+    if os.path.exists('current.csv'):
+        os.remove('current.csv')
+    os.rename('newest.csv','current.csv')
 
 
-
-<<<<<<< HEAD
-=======
-
->>>>>>> 9d9e9b1a6e1ed6902bb98150ad3cde4a4033c7aa
 if __name__ == '__main__':
     main()
